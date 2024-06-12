661799462aebe8409386bbae328734e97bb381c9
Roger <haidx@avada.io>
2024-06-12 11:14:04 +0700

    update

---
+++ TS/check3.js
@@ -0,0 +1,657 @@
+{
+    contacts: 0,
+    contact_limit: 500,
+    contact_limit_1: 2500,
+    pro_price: 16,
+    advanced_price: 47,
+    pro_emails: 12000,
+    advanced_emails: 48000
+  },
+  {
+    contacts: 1000,
+    contact_limit: 1000,
+    contact_limit_1: 2500,
+    pro_price: 20,
+    advanced_price: 47,
+    pro_emails: 18000,
+    advanced_emails: 48000
+  },
+  {
+    contacts: 1500,
+    contact_limit: 1500,
+    contact_limit_1: 2500,
+    pro_price: 24,
+    advanced_price: 47,
+    pro_emails: 24000,
+    advanced_emails: 48000
+  },
+  {
+    contacts: 2000,
+    contact_limit: 2000,
+    contact_limit_1: 2500,
+    pro_price: 28,
+    advanced_price: 47,
+    pro_emails: 30000,
+    advanced_emails: 48000
+  },
+  {
+    contacts: 2500,
+    contact_limit: 2500,
+    contact_limit_1: 2500,
+    pro_price: 32,
+    advanced_price: 56,
+    pro_emails: 36000,
+    advanced_emails: 60000
+  },
+  {
+    contacts: 3000,
+    contact_limit: 3000,
+    contact_limit_1: 3000,
+    pro_price: 36,
+    advanced_price: 60,
+    pro_emails: 42000,
+    advanced_emails: 72000
+  },
+  {
+    contacts: 3500,
+    contact_limit: 3500,
+    contact_limit_1: 3500,
+    pro_price: 40,
+    advanced_price: 64,
+    pro_emails: 48000,
+    advanced_emails: 84000
+  },
+  {
+    contacts: 4000,
+    contact_limit: 4000,
+    contact_limit_1: 4000,
+    pro_price: 48,
+    advanced_price: 68,
+    pro_emails: 54000,
+    advanced_emails: 96000
+  },
+  {
+    contacts: 4500,
+    contact_limit: 4500,
+    contact_limit_1: 4500,
+    pro_price: 52,
+    advanced_price: 72,
+    pro_emails: 60000,
+    advanced_emails: 108000
+  },
+  {
+    contacts: 5000,
+    contact_limit: 5000,
+    contact_limit_1: 5000,
+    pro_price: 56,
+    advanced_price: 84,
+    pro_emails: 66000,
+    advanced_emails: 120000
+  },
+  {
+    contacts: 5500,
+    contact_limit: 5500,
+    contact_limit_1: 5500,
+    pro_price: 64,
+    advanced_price: 92,
+    pro_emails: 72000,
+    advanced_emails: 132000
+  },
+  {
+    contacts: 6000,
+    contact_limit: 6000,
+    contact_limit_1: 6000,
+    pro_price: 68,
+    advanced_price: 100,
+    pro_emails: 78000,
+    advanced_emails: 144000
+  },
+  {
+    contacts: 6500,
+    contact_limit: 6500,
+    contact_limit_1: 6500,
+    pro_price: 72,
+    advanced_price: 108,
+    pro_emails: 84000,
+    advanced_emails: 156000
+  },
+  {
+    contacts: 7000,
+    contact_limit: 7000,
+    contact_limit_1: 7000,
+    pro_price: 88,
+    advanced_price: 116,
+    pro_emails: 108000,
+    advanced_emails: 168000
+  },
+  {
+    contacts: 9000,
+    contact_limit: 9000,
+    contact_limit_1: 9000,
+    pro_price: 92,
+    advanced_price: 120,
+    pro_emails: 120000,
+    advanced_emails: 216000
+  },
+  {
+    contacts: 10000,
+    contact_limit: 10000,
+    contact_limit_1: 10000,
+    pro_price: 100,
+    advanced_price: 136,
+    pro_emails: 126000,
+    advanced_emails: 240000
+  },
+  {
+    contacts: 10500,
+    contact_limit: 10500,
+    contact_limit_1: 10500,
+    pro_price: 104,
+    advanced_price: 152,
+    pro_emails: 132000,
+    advanced_emails: 252000
+  },
+  {
+    contacts: 11000,
+    contact_limit: 11000,
+    contact_limit_1: 11000,
+    pro_price: 108,
+    advanced_price: 168,
+    pro_emails: 138000,
+    advanced_emails: 264000
+  },
+  {
+    contacts: 11500,
+    contact_limit: 11500,
+    contact_limit_1: 11500,
+    pro_price: 112,
+    advanced_price: 176,
+    pro_emails: 144000,
+    advanced_emails: 276000
+  },
+  {
+    contacts: 12000,
+    contact_limit: 12000,
+    contact_limit_1: 12000,
+    pro_price: 120,
+    advanced_price: 192,
+    pro_emails: 150000,
+    advanced_emails: 288000
+  },
+  {
+    contacts: 12500,
+    contact_limit: 12500,
+    contact_limit_1: 12500,
+    pro_price: 128,
+    advanced_price: 200,
+    pro_emails: 156000,
+    advanced_emails: 300000
+  },
+  {
+    contacts: 13000,
+    contact_limit: 13000,
+    contact_limit_1: 13000,
+    pro_price: 132,
+    advanced_price: 208,
+    pro_emails: 162000,
+    advanced_emails: 312000
+  },
+  {
+    contacts: 13500,
+    contact_limit: 13500,
+    contact_limit_1: 13500,
+    pro_price: 144,
+    advanced_price: 232,
+    pro_emails: 180000,
+    advanced_emails: 324000
+  },
+  {
+    contacts: 15000,
+    contact_limit: 15000,
+    contact_limit_1: 15000,
+    pro_price: 152,
+    advanced_price: 248,
+    pro_emails: 192000,
+    advanced_emails: 360000
+  },
+  {
+    contacts: 16000,
+    contact_limit: 16000,
+    contact_limit_1: 16000,
+    pro_price: 160,
+    advanced_price: 264,
+    pro_emails: 204000,
+    advanced_emails: 384000
+  },
+  {
+    contacts: 17000,
+    contact_limit: 17000,
+    contact_limit_1: 17000,
+    pro_price: 176,
+    advanced_price: 272,
+    pro_emails: 222000,
+    advanced_emails: 408000
+  },
+  {
+    contacts: 18500,
+    contact_limit: 18500,
+    contact_limit_1: 18500,
+    pro_price: 184,
+    advanced_price: 280,
+    pro_emails: 240000,
+    advanced_emails: 444000
+  },
+  {
+    contacts: 20000,
+    contact_limit: 20000,
+    contact_limit_1: 20000,
+    pro_price: 188,
+    advanced_price: 288,
+    pro_emails: 258000,
+    advanced_emails: 480000
+  },
+  {
+    contacts: 21500,
+    contact_limit: 21500,
+    contact_limit_1: 21500,
+    pro_price: 192,
+    advanced_price: 292,
+    pro_emails: 282000,
+    advanced_emails: 516000
+  },
+  {
+    contacts: 23500,
+    contact_limit: 23500,
+    contact_limit_1: 23500,
+    pro_price: 196,
+    advanced_price: 296,
+    pro_emails: 300000,
+    advanced_emails: 564000
+  },
+  {
+    contacts: 25000,
+    contact_limit: 25000,
+    contact_limit_1: 25000,
+    pro_price: 200,
+    advanced_price: 312,
+    pro_emails: 312000,
+    advanced_emails: 600000
+  },
+  {
+    contacts: 26000,
+    contact_limit: 26000,
+    contact_limit_1: 26000,
+    pro_price: 204,
+    advanced_price: 328,
+    pro_emails: 324000,
+    advanced_emails: 624000
+  },
+  {
+    contacts: 27000,
+    contact_limit: 27000,
+    contact_limit_1: 27000,
+    pro_price: 208,
+    advanced_price: 344,
+    pro_emails: 336000,
+    advanced_emails: 648000
+  },
+  {
+    contacts: 28000,
+    contact_limit: 28000,
+    contact_limit_1: 28000,
+    pro_price: 212,
+    advanced_price: 356,
+    pro_emails: 348000,
+    advanced_emails: 672000
+  },
+  {
+    contacts: 29000,
+    contact_limit: 29000,
+    contact_limit_1: 29000,
+    pro_price: 216,
+    advanced_price: 368,
+    pro_emails: 360000,
+    advanced_emails: 696000
+  },
+  {
+    contacts: 30000,
+    contact_limit: 30000,
+    contact_limit_1: 30000,
+    pro_price: 220,
+    advanced_price: 384,
+    pro_emails: 372000,
+    advanced_emails: 720000
+  },
+  {
+    contacts: 31000,
+    contact_limit: 31000,
+    contact_limit_1: 31000,
+    pro_price: 224,
+    advanced_price: 392,
+    pro_emails: 384000,
+    advanced_emails: 744000
+  },
+  {
+    contacts: 32000,
+    contact_limit: 32000,
+    contact_limit_1: 32000,
+    pro_price: 228,
+    advanced_price: 424,
+    pro_emails: 420000,
+    advanced_emails: 768000
+  },
+  {
+    contacts: 35000,
+    contact_limit: 35000,
+    contact_limit_1: 35000,
+    pro_price: 232,
+    advanced_price: 440,
+    pro_emails: 450000,
+    advanced_emails: 840000
+  },
+  {
+    contacts: 37500,
+    contact_limit: 37500,
+    contact_limit_1: 37500,
+    pro_price: 240,
+    advanced_price: 456,
+    pro_emails: 480000,
+    advanced_emails: 900000
+  },
+  {
+    contacts: 40000,
+    contact_limit: 40000,
+    contact_limit_1: 40000,
+    pro_price: 248,
+    advanced_price: 480,
+    pro_emails: 540000,
+    advanced_emails: 960000
+  },
+  {
+    contacts: 45000,
+    contact_limit: 45000,
+    contact_limit_1: 45000,
+    pro_price: 264,
+    advanced_price: 520,
+    pro_emails: 600000,
+    advanced_emails: 1080000
+  },
+  {
+    contacts: 50000,
+    contact_limit: 50000,
+    contact_limit_1: 50000,
+    pro_price: 288,
+    advanced_price: 560,
+    pro_emails: 660000,
+    advanced_emails: 1200000
+  },
+  {
+    contacts: 55000,
+    contact_limit: 55000,
+    contact_limit_1: 55000,
+    pro_price: 320,
+    advanced_price: 600,
+    pro_emails: 720000,
+    advanced_emails: 1320000
+  },
+  {
+    contacts: 60000,
+    contact_limit: 60000,
+    contact_limit_1: 60000,
+    pro_price: 352,
+    advanced_price: 640,
+    pro_emails: 780000,
+    advanced_emails: 1440000
+  },
+  {
+    contacts: 65000,
+    contact_limit: 65000,
+    contact_limit_1: 65000,
+    pro_price: 384,
+    advanced_price: 680,
+    pro_emails: 840000,
+    advanced_emails: 1560000
+  },
+  {
+    contacts: 70000,
+    contact_limit: 70000,
+    contact_limit_1: 70000,
+    pro_price: 416,
+    advanced_price: 720,
+    pro_emails: 900000,
+    advanced_emails: 1680000
+  },
+  {
+    contacts: 75000,
+    contact_limit: 75000,
+    contact_limit_1: 75000,
+    pro_price: 448,
+    advanced_price: 760,
+    pro_emails: 960000,
+    advanced_emails: 1800000
+  },
+  {
+    contacts: 80000,
+    contact_limit: 80000,
+    contact_limit_1: 80000,
+    pro_price: 480,
+    advanced_price: 800,
+    pro_emails: 1020000,
+    advanced_emails: 1920000
+  },
+  {
+    contacts: 85000,
+    contact_limit: 85000,
+    contact_limit_1: 85000,
+    pro_price: 512,
+    advanced_price: 840,
+    pro_emails: 1080000,
+    advanced_emails: 2040000
+  },
+  {
+    contacts: 90000,
+    contact_limit: 90000,
+    contact_limit_1: 90000,
+    pro_price: 544,
+    advanced_price: 880,
+    pro_emails: 1140000,
+    advanced_emails: 2160000
+  },
+  {
+    contacts: 95000,
+    contact_limit: 95000,
+    contact_limit_1: 95000,
+    pro_price: 576,
+    advanced_price: 920,
+    pro_emails: 1200000,
+    advanced_emails: 2280000
+  },
+  {
+    contacts: 100000,
+    contact_limit: 100000,
+    contact_limit_1: 100000,
+    pro_price: 608,
+    advanced_price: 960,
+    pro_emails: 1260000,
+    advanced_emails: 2400000
+  },
+  {
+    contacts: 105000,
+    contact_limit: 105000,
+    contact_limit_1: 105000,
+    pro_price: 640,
+    advanced_price: 1000,
+    pro_emails: 1320000,
+    advanced_emails: 2520000
+  },
+  {
+    contacts: 110000,
+    contact_limit: 110000,
+    contact_limit_1: 110000,
+    pro_price: 672,
+    advanced_price: 1040,
+    pro_emails: 1380000,
+    advanced_emails: 2640000
+  },
+  {
+    contacts: 115000,
+    contact_limit: 115000,
+    contact_limit_1: 115000,
+    pro_price: 704,
+    advanced_price: 1080,
+    pro_emails: 1440000,
+    advanced_emails: 2760000
+  },
+  {
+    contacts: 120000,
+    contact_limit: 120000,
+    contact_limit_1: 120000,
+    pro_price: 736,
+    advanced_price: 1120,
+    pro_emails: 1500000,
+    advanced_emails: 2880000
+  },
+  {
+    contacts: 125000,
+    contact_limit: 125000,
+    contact_limit_1: 125000,
+    pro_price: 768,
+    advanced_price: 1160,
+    pro_emails: 1560000,
+    advanced_emails: 3000000
+  },
+  {
+    contacts: 130000,
+    contact_limit: 130000,
+    contact_limit_1: 130000,
+    pro_price: 800,
+    advanced_price: 1200,
+    pro_emails: 1620000,
+    advanced_emails: 3120000
+  },
+  {
+    contacts: 135000,
+    contact_limit: 135000,
+    contact_limit_1: 135000,
+    pro_price: 832,
+    advanced_price: 1240,
+    pro_emails: 1680000,
+    advanced_emails: 3240000
+  },
+  {
+    contacts: 140000,
+    contact_limit: 140000,
+    contact_limit_1: 140000,
+    pro_price: 864,
+    advanced_price: 1280,
+    pro_emails: 1740000,
+    advanced_emails: 3360000
+  },
+  {
+    contacts: 145000,
+    contact_limit: 145000,
+    contact_limit_1: 145000,
+    pro_price: 896,
+    advanced_price: 1320,
+    pro_emails: 1800000,
+    advanced_emails: 3480000
+  },
+  {
+    contacts: 150000,
+    contact_limit: 150000,
+    contact_limit_1: 150000,
+    pro_price: 928,
+    advanced_price: 1360,
+    pro_emails: 1860000,
+    advanced_emails: 3600000
+  },
+  {
+    contacts: 155000,
+    contact_limit: 155000,
+    contact_limit_1: 155000,
+    pro_price: 952,
+    advanced_price: 1400,
+    pro_emails: 1920000,
+    advanced_emails: 3720000
+  },
+  {
+    contacts: 160000,
+    contact_limit: 160000,
+    contact_limit_1: 160000,
+    pro_price: 976,
+    advanced_price: 1440,
+    pro_emails: 1980000,
+    advanced_emails: 3840000
+  },
+  {
+    contacts: 165000,
+    contact_limit: 165000,
+    contact_limit_1: 165000,
+    pro_price: 1000,
+    advanced_price: 1480,
+    pro_emails: 2040000,
+    advanced_emails: 3960000
+  },
+  {
+    contacts: 170000,
+    contact_limit: 170000,
+    contact_limit_1: 170000,
+    pro_price: 1024,
+    advanced_price: 1520,
+    pro_emails: 2100000,
+    advanced_emails: 4080000
+  },
+  {
+    contacts: 175000,
+    contact_limit: 175000,
+    contact_limit_1: 175000,
+    pro_price: 1048,
+    advanced_price: 1560,
+    pro_emails: 2160000,
+    advanced_emails: 4200000
+  },
+  {
+    contacts: 180000,
+    contact_limit: 180000,
+    contact_limit_1: 180000,
+    pro_price: 1072,
+    advanced_price: 1600,
+    pro_emails: 2220000,
+    advanced_emails: 4320000
+  },
+  {
+    contacts: 185000,
+    contact_limit: 185000,
+    contact_limit_1: 185000,
+    pro_price: 1096,
+    advanced_price: 1640,
+    pro_emails: 2280000,
+    advanced_emails: 4440000
+  },
+  {
+    contacts: 190000,
+    contact_limit: 190000,
+    contact_limit_1: 190000,
+    pro_price: 1120,
+    advanced_price: 1680,
+    pro_emails: 2340000,
+    advanced_emails: 4560000
+  },
+  {
+    contacts: 195000,
+    contact_limit: 195000,
+    contact_limit_1: 195000,
+    pro_price: 1144,
+    advanced_price: 1720,
+    pro_emails: 2400000,
+    advanced_emails: 4680000
+  },
+  {
+    contacts: 200000,
+    contact_limit: 200000,
+    contact_limit_1: 200000,
+    pro_price: 1192,
+    advanced_price: 1800,
+    pro_emails: 2520000,
+    advanced_emails: 4800000
+  }
